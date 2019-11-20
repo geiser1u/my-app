767c75b626ec9ad9dfb23b22cbd6e1846b12065e
geiser1u <sylvain.geiser2@etu.univ-lorraine.fr>
2019-11-20 15:34:51 +0100

    Done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'coucou' });
 })
 
-app.listen(80, (err) => {
+app.listen(PORT, (err) => {
     if (err) {
         console.error(err);
         process.exit(1);
