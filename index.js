c97a99c6d60f50efe980e03eb0ff9c50aeef89b3
geiser1u <sylvain.geiser2@etu.univ-lorraine.fr>
2019-11-20 15:47:39 +0100

    Done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'coucou' });
+    res.json({ message: 'coucou moi', title: 'Coucou' });
 })
 
 app.listen(PORT, (err) => {
