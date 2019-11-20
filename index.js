0b224c4e55716e82d31677479b51ccb504084419
geiser1u <43879821+geiser1u@users.noreply.github.com>
2019-11-20 16:22:26 +0100

    Done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'coucou moi', title: 'Coucou' });
+    res.json({ message: 'coucou moi' });
 })
 
 app.listen(PORT, (err) => {
